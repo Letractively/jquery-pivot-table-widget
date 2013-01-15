1afa2c6e8109bf4e736b3f24b141be8df5572524
sanjay1982@gmail.com <sanjay1982@gmail.com@26451bfd-8d9e-6ef0-477e-40b2429addfb>
2013-01-15 16:33:18 +0000

    Changed mime type

--- trunk/PivotTable.js
+++ trunk/PivotTable.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-1.8.3.js" />
+/// <reference path="jquery-1.8.3.js" />
 /// <reference path="jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js" />
 (function ($, undefined) {
 
